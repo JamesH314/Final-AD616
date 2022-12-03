a10bbbfe34be1ccb0c4e4da19cdd5fd9cc4b7857
James <yfhe@bu.edu>
2022-12-03 15:15:00 -0500

    simulation

--- James' Code.R
+++ James' Code.R
@@ -1,7 +1,7 @@
 # Initial place for coding by James
 
 Peak <- read.csv("PeakData.csv", header = T)
-Peak_1 <- Peak[-1,]
+Peak_1 <- Peak[-1,-1]
 View(Peak_1)
 
 NonPeak <- read.csv("NonPeakData.csv", header = T)
@@ -11,3 +11,18 @@ View(NonPeak_1)
 #useful functions
 upper.tri()
 rowSums()
+
+#coding
+
+p_1 <- unlist(as.list(t(Peak_1)))
+p_1
+
+PeakSim <- c(rep(0,441))
+for (i in 1:441) {
+  PeakSim[i] <- mean(rnorm(10000, mean = p_1[i], sd = p_1[i]/5))
+}
+
+PS_1 <- matrix(data = PeakSim, nrow = 21, ncol = 21)
+PS_1[upper.tri(PS_1, diag = F)]/4
+
+
