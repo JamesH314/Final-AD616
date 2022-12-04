3ea33b200f5d1a9317ed31250770cb044cf2af84
James <yfhe@bu.edu>
2022-12-04 16:02:43 -0500

    more graph

--- James' Code.R
+++ James' Code.R
@@ -1,5 +1,7 @@
 ## Initial place for coding by James
 library(ggplot2)
+library(ggridges)
+library(tidyr)
 
 ##Peak hours, RG final stop
 Peak <- read.csv("PeakData.csv", header = T)
@@ -12,10 +14,23 @@ Peak_RG1 <- cbind(Peak_RG, c(rowSums(Peak_RG)))
 Peak_RG2 <- rbind(Peak_RG1, c(colSums(Peak_RG1)))
 #View(Peak_RG2)
 
-#sum of people get on the bus at each stop
+#sum of people get off the bus at each stop
 P_RG2 <- data.frame(val = Peak_RG2[-22,22], name = colnames(Peak_RG))
-Sum_GetOn1 <- ggplot(data = P_RG2, aes(x = factor(name, level=
+Sum_GetOff1 <- ggplot(data = P_RG2, aes(x = factor(name, level=
                                                    c('Rajiv.Gandhi','Mata.Gujri','Vishnupuri','Bhanwarkuan.Square','Holkar.Subway','Navlakha.Square',
+                                                      'Indra.Pratima','GPO','Shivaji.Vatika','AICTSL','Geeta.Bhawan','Palasiya',
+                                                      'Industry.House','LIG','Press.Complex','MR.9','Vijay.Nagar','Satya.Sai',
+                                                      'Shalimar.Township','Scheme.No..78','Niranjanpur.Square')), y = val)) + 
+  geom_col(width = 0.01, color = "black") +
+  geom_point(size = 3, color = "red", fill = alpha("orange", 0.3), alpha = 0.7, shape = 21, stroke = 2)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+print(Sum_GetOff1 + ggtitle("Sum of passengers get off the bus at each stop") + 
+        labs(y = "Number of Passenger", x = "Bus Stop"))
+
+#sum of people get on the bus at each stop
+P_RG3 <- data.frame(val = unlist(Peak_RG2[22,-22]), name = colnames(Peak_RG))
+Sum_GetOn1 <- ggplot(data = P_RG3, aes(x = factor(name, level=
+                                                    c('Rajiv.Gandhi','Mata.Gujri','Vishnupuri','Bhanwarkuan.Square','Holkar.Subway','Navlakha.Square',
                                                       'Indra.Pratima','GPO','Shivaji.Vatika','AICTSL','Geeta.Bhawan','Palasiya',
                                                       'Industry.House','LIG','Press.Complex','MR.9','Vijay.Nagar','Satya.Sai',
                                                       'Shalimar.Township','Scheme.No..78','Niranjanpur.Square')), y = val)) + 
@@ -24,6 +39,23 @@ Sum_GetOn1 <- ggplot(data = P_RG2, aes(x = factor(name, level=
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 print(Sum_GetOn1 + ggtitle("Sum of passengers get on the bus at each stop") + 
         labs(y = "Number of Passenger", x = "Bus Stop"))
+
+#Sum of on and off
+P_RG4 <- data.frame(val = unlist(c(unlist(Peak_RG2[22,-22]), Peak_RG2[-22,22])), 
+                    name = rep(colnames(Peak_RG),2), 
+                    on_off = c(rep("off", 21), rep("on",21)))
+Sum_1 <- ggplot(data = P_RG4, aes(x = factor(name, level=
+                                                    c('Rajiv.Gandhi','Mata.Gujri','Vishnupuri','Bhanwarkuan.Square','Holkar.Subway','Navlakha.Square',
+                                                      'Indra.Pratima','GPO','Shivaji.Vatika','AICTSL','Geeta.Bhawan','Palasiya',
+                                                      'Industry.House','LIG','Press.Complex','MR.9','Vijay.Nagar','Satya.Sai',
+                                                      'Shalimar.Township','Scheme.No..78','Niranjanpur.Square')), 
+                                  y = val, fill = on_off)) + 
+  geom_bar(position = "dodge", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+print(Sum_1 + ggtitle("Sum of passengers get on and off the bus at each stop") + 
+        labs(y = "Number of Passenger", x = "Bus Stop"))
+
+
 
 ##Peak hours, NS final stop
 Peak_NS <- Peak_1
